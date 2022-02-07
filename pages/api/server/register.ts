0a89226af6b11a8ee72b9cb1603dfb23c1a98f67
UnRealReincarlution <benwhite2413@gmail.com>
2022-02-07 13:08:28 +1300

    Detailed Error Checking and Response Hander for Server Registry

--- pages/api/server/register.ts
+++ pages/api/server/register.ts
@@ -1,18 +1,85 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '@root/lib/prisma'
 
+type Server = {
+    id: string,
+    location: string,
+    country: string,
+    virtual: string,
+    flag: string,
+    hostname: string,
+    override?: string // re-instantiate server instead of soft-pickup.
+}
+
 // POST /server/register
 // Required fields in body: id, location, country, hostname, flag
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-    const bod = req.body;
-    const result = await prisma.server.create({
-        data: {
-            ...bod,
-            virtual: req.body.virtual == 'true' ? true : false,
-        },
+    const { override, ...json }: Server = await JSON.parse(req.body);
+    const server_exists = await prisma.server.findFirst({
+        where: {
+            OR: [
+                {
+                    id: json.id
+                },
+                {
+                    hostname: json.hostname
+                }
+            ]
+        }
     });
-
-    console.log(result);
     
-    res.json(result)
+    if(!server_exists) {
+        console.log("Creating New Server");
+
+        console.log({
+            ...json,
+            virtual: json.virtual == 'true' ? true : false,
+        });
+
+        const result = await prisma.server.create({
+            data: {
+                ...json,
+                virtual: json.virtual == 'true' ? true : false,
+            },
+        }).catch(e => {
+            console.log(e);
+            res.json({
+                error: e,
+                reason: "See Error Object",
+                data: result
+            })
+        })
+    
+        console.log(result);
+
+        res.json({
+            error: "",
+            reason: "Success - Updated",
+            data: result
+        });
+    }else if(override == 'true') {
+        const result = await prisma.server.update({
+            where: {
+                id: json.id
+            },
+            data: {
+                ...json,
+                virtual: json.virtual == 'true' ? true : false,
+            },
+        });
+    
+        console.log(result);
+
+        res.json({
+            error: "",
+            reason: "Success - Overridden",
+            data: result
+        });
+    }else {
+        res.json({
+            error: "failure",
+            reason: "Server Exists - Pass Override Flag to override existing server.",
+            data: {}
+        })
+    }
 }
