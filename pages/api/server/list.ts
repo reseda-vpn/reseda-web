de74cdf8fd6f6081731f0fc502357aabe5e2d645
UnRealReincarlution <benwhite2413@gmail.com>
2022-02-08 20:12:34 +1300

    CORS Policy

--- pages/api/server/list.ts
+++ pages/api/server/list.ts
@@ -14,9 +14,9 @@ type Server = {
 // POST /server/register
 // Required fields in body: id, location, country, hostname, flag
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET')
+    // res.setHeader('Access-Control-Allow-Credentials', 'true');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET')
 
     const server_pool = await prisma.server.findMany({
         where: {
