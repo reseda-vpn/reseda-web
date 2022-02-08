8c4775ef3c71402076bff14ebc2ee44f4c0777ef
UnRealReincarlution <benwhite2413@gmail.com>
2022-02-08 19:21:17 +1300

    Server List

---
+++ pages/api/server/list.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@root/lib/prisma'
+
+type Server = {
+    id: string,
+    location: string,
+    country: string,
+    virtual: string,
+    flag: string,
+    hostname: string,
+    override?: string // re-instantiate server instead of soft-pickup.
+}
+
+// POST /server/register
+// Required fields in body: id, location, country, hostname, flag
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+    const server_pool = await prisma.server.findMany({
+        where: {
+            virtual: false
+        }
+    });
+
+    res.json(server_pool);
+}
