0f85cf4d21b7fccec98d7448194205c8b05d2b40
UnRealReincarlution <benwhite2413@gmail.com>
2022-10-07 22:25:42 +1300

    This-Month API route

--- pages/api/user/usage/this-month/[userId].ts
+++ pages/api/user/usage/this-month/[userId].ts
@@ -9,7 +9,7 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     const result = await prisma.usage.findMany({
         where: {
             userId: userId.toString(),
-            connStart: {
+            connEnd: {
                 gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1)
             }
         }
